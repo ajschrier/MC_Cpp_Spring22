f1b07636e256aa309545d94cf553d688103f99fa
AJ Schrier <aschrier@apple.com>
2022-02-21 23:03:33 -0800

    Project for Lab 4

---
+++ Lab4/Lab4/Lab4/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Lab4
+//
+//  Created by AJ Schrier on 2/21/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
